712bebf1030a8bc2d6cee235e927727178df6ac5
o.gryshchuk <o.gryshchuk@foodtech.team>
2025-01-30 21:21:39 +0200

    add http client and server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/grischuk-sasha/golang
+
+go 1.23.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
---
+++ http/http_server/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	createNoteURL = "/notes"
+	getNoteURL    = "/notes/{id}"
+)
+
+type NoteInfo struct {
+	Title    string `json:"title"`
+	Context  string `json:"context"`
+	Author   string `json:"author"`
+	IsPublic bool   `json:"is_public"`
+}
+
+type Note struct {
+	ID        int64     `json:"id"`
+	Info      NoteInfo  `json:"info"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type SyncMap struct {
+	elems map[int64]*Note
+	m     sync.RWMutex
+}
+
+var notes = &SyncMap{
+	elems: make(map[int64]*Note),
+}
+
+func createNoteHandler(w http.ResponseWriter, r *http.Request) {
+	info := &NoteInfo{}
+
+	if err := json.NewDecoder(r.Body).Decode(info); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if info.Title == "" || info.Author == "" {
+		http.Error(w, "Title and Author are required", http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now()
+
+	note := &Note{
+		ID:        rand.Int63(),
+		Info:      *info,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(note); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	notes.m.Lock()
+	defer notes.m.Unlock()
+
+	notes.elems[note.ID] = note
+
+	log.Printf("Created note: ID=%d, Title=%s", note.ID, note.Info.Title)
+}
+
+func getNoteHandler(w http.ResponseWriter, r *http.Request) {
+	noteID := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(noteID, 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid note ID", http.StatusBadRequest)
+		return
+	}
+
+	notes.m.RLock()
+	defer notes.m.RUnlock()
+
+	note, ok := notes.elems[id]
+	if !ok {
+		http.Error(w, "Note not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(note); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	addr := flag.String("addr", "localhost:8081", "HTTP server address")
+	flag.Parse()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	r.Post(createNoteURL, createNoteHandler)
+	r.Get(getNoteURL, getNoteHandler)
+
+	log.Printf("Server running on %s", *addr)
+	if err := http.ListenAndServe(*addr, r); err != nil {
+		log.Fatal(err)
+	}
+}
